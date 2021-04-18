dbbf55d6ea965125d8ada20de7d445637cb9c845
John Alalade <johnalalade6@gmail.com>
2021-04-18 15:00:05 -0400

    Desktop View Width

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -23,92 +23,93 @@ export default class Home extends Component {
 
 
     pagination = (pagename) => {
-        this.setState({page: pagename})
+        this.setState({ page: pagename })
     }
 
     render() {
         return (
-            <div>
-                <Header />
+            <div className="outter-container">
+                <div className="content">
+                    <Header />
 
-                {(this.state.page !== "Home")? <Page page={this.state.page} /> :
-                    <div className="outter-container">
-                        <div className="content">
-                        {/* Top Box */}
-                        <div className="box-container">
-                            <div className="AFYA">
-                                <div className="border-radius">
-                                    <h6 className="name1">AFYA</h6>
+                    {(this.state.page !== "Home") ? <Page page={this.state.page} /> :
+                        <div>
+                            {/* Top Box */}
+                            <div className="box-container">
+                                <div className="AFYA">
+                                    <div className="border-radius">
+                                        <h6 className="name1">AFYA</h6>
+                                    </div>
+                                    <p className="number">143.00</p>
+                                    <p className="text">Balance</p>
                                 </div>
-                                <p className="number">143.00</p>
-                                <p className="text">Balance</p>
-                            </div>
 
-                            <div className="line"></div>
+                                <div className="line"></div>
 
-                            <div className="ZAR">
-                                <div className="border-radius">
-                                    <h6 className="name2">ZAR</h6>
+                                <div className="ZAR">
+                                    <div className="border-radius">
+                                        <h6 className="name2">ZAR</h6>
+                                    </div>
+                                    <p className="number">R21.00</p>
+                                    <p className="text">Equivalent</p>
                                 </div>
-                                <p className="number">R21.00</p>
-                                <p className="text">Equivalent</p>
                             </div>
-                        </div>
 
-                        {/* Currency */}
-                        <br />
-                        <div className="currency">
-                            <p className="select-currency">Select Currency:</p>
-                        </div>
-
-                        <div className="select currency">
-
-                            <select value={this.state.currency} name="currency" onChange={this.currency} className="select-box">
-                                {this.state.currencies.map((cur) =>
-                                    <option key={cur} value={cur}>{cur}</option>
-                                )}
-
-                            </select>
-
-                            <p className="digits">0.00000001</p>
-                        </div>
-
-                        {/* Buttons */}
-                        <div className="buttons">
-                            <button className="btn btn-primary">SEND</button>
-                            <button className="btn btn-primary">RECEIVE</button>
-                        </div>
-
-                        {/* Recent Activity */}
-                        <div className="recent-bg">
-                            <p className="recent">Recent Activity</p>
-                        </div>
-                        {/* lists */}
-                        <div className="lists">
-                            <div className="list">
-                                <img src={Aids} className="list-img" alt="HIV" />
-                                <p className="list-text">HIV Blood Draw Test</p>
-                                <p className="list-price">A100</p>
+                            {/* Currency */}
+                            <br />
+                            <div className="currency">
+                                <p className="select-currency">Select Currency:</p>
                             </div>
-                            <div className="list">
-                                <img src={lungs} className="list-img" alt="HIV" />
-                                <p className="list-text">TB Radiology XRay</p>
-                                <p className="list-price">A30</p>
-                            </div>
-                            <div className="list">
-                                <img src={Bp} className="list-img" alt="HIV" />
-                                <p className="list-text">Blood Pressure Check</p>
-                                <p className="list-price">A10</p>
-                            </div>
-                        </div>
 
-                        {/* Last Bar */}
-                        <div className="last-bar"></div>
+                            <div className="select currency">
 
-                        <br />
-                        </div>
-                    </div>}
-                <Footer pagename={this.state.page} pagination={this.pagination} />
+                                <select value={this.state.currency} name="currency" onChange={this.currency} className="select-box">
+                                    {this.state.currencies.map((cur) =>
+                                        <option key={cur} value={cur}>{cur}</option>
+                                    )}
+
+                                </select>
+
+                                <p className="digits">0.00000001</p>
+                            </div>
+
+                            {/* Buttons */}
+                            <div className="buttons">
+                                <button className="btn btn-primary">SEND</button>
+                                <button className="btn btn-primary">RECEIVE</button>
+                            </div>
+
+                            {/* Recent Activity */}
+                            <div className="recent-bg">
+                                <p className="recent">Recent Activity</p>
+                            </div>
+                            {/* lists */}
+                            <div className="lists">
+                                <div className="list">
+                                    <img src={Aids} className="list-img" alt="HIV" />
+                                    <p className="list-text">HIV Blood Draw Test</p>
+                                    <p className="list-price">A100</p>
+                                </div>
+                                <div className="list">
+                                    <img src={lungs} className="list-img" alt="HIV" />
+                                    <p className="list-text">TB Radiology XRay</p>
+                                    <p className="list-price">A30</p>
+                                </div>
+                                <div className="list">
+                                    <img src={Bp} className="list-img" alt="HIV" />
+                                    <p className="list-text">Blood Pressure Check</p>
+                                    <p className="list-price">A10</p>
+                                </div>
+                            </div>
+
+                            {/* Last Bar */}
+                            <div className="last-bar"></div>
+
+                            <br />
+
+                        </div>}
+                    <Footer pagename={this.state.page} pagination={this.pagination} />
+                </div>
             </div>
         )
     }
